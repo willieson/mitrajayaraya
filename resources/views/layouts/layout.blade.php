2e35195f18d528b9cc6027ef954ac09c9ccfed56
Michael Willieson - laptop <michaelwillieson@gmail.com>
2024-10-18 10:55:09 +0700

    Upload first

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TokoSoul - {{ $title }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Header / Navbar -->
+    @include('components.nav-bar')
+
+    <!-- Konten utama halaman -->
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+    <!-- Footer -->
+    @include('components.footer')
+</body>
+
+</html>
